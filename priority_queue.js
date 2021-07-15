dbf95f0b2278b3059439b23e70da0f408bd6b4e0
Victor Aluko <valuko@users.noreply.github.com>
2021-07-15 01:11:32 -0500

    Updated priority queue with comments of changes

--- priority_queue.js
+++ priority_queue.js
@@ -1,15 +1,25 @@
+/**
+ * Converted to a class to meet the JS convention of standard classes over Prototype classes
+ */
 class PriorityQueue {
+    /** Removed the size parameter since it was not being used anywhere in the constructor */
     constructor() {
         this.store = {};
         this.count = 0;
     }
 
     add(value, priority) {
+        /**
+         * Added validation of the priority type to be an integer
+         * Throw an error if its not an integer
+         */
         if (isNaN(parseInt(priority))) {
-            throw new Error('Priority is not a number');
+            throw new Error('Priority is not an integer');
         }
 
+        /** Used the integer value of priority for consistency */
         priority = parseInt(priority);
+        /** Changed the check here to use typeof and === which is safer than == check */
         if (typeof this.store[priority] === "undefined") {
             this.store[priority] = [];
         }
@@ -19,6 +29,12 @@ class PriorityQueue {
     }
 
     pop() {
+        /** Return null if no elements currently in the store */
+        if (this.count < 1) {
+            return null;
+        }
+
+        /** Added const before maxKey. Bad practice not to have const/var/let before a variable declaration */
         const maxKey = Math.max(Object.keys(this.store));
         this.count--;
         return this.store[maxKey].shift();
@@ -28,13 +44,15 @@ class PriorityQueue {
         return this.count;
     }
 
+    /** Declared as a getter since it only returns a value. Camel cased method declaration also */
     get allPriorities() {
         return Object.keys(this.store);
     }
 
     forEach(callback) {
-        const keys = this.allPriorities.sort();
+        const keys = Object.keys(this.store).sort();
 
+        /** Used for-of because the priority keys may not be ordered as previously used */
         for (let key of keys) {
             for (let b = 0; b < this.store[key].length; b++) {
                 callback(this.store[key][b]);
@@ -43,6 +61,13 @@ class PriorityQueue {
     }
 
     changePriority(value, newPriority) {
+        /** 
+         * This method assumes a value can only have one priority 
+         * However, the add method could add multiple priorities to a value
+         * A more suitable name for this function would be 'changeFirstPriority' 
+         *  or preventing add() from adding a value to multiple priorities
+         */
+        /** Changed from a forEach to a for since it is easier to break out of a for loop than a forEach*/
         for (let priority in this.store) {
             for (let i = 0; i < this.store[priority].length; i++) {
                 if (this.store[priority][i] === value) {
@@ -53,4 +78,6 @@ class PriorityQueue {
             }
         }
     }
+    
+    /** It would be helpful to have peek() and findPriority() methods */
 }
